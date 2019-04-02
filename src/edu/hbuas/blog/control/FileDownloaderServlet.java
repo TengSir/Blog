f37b46cbbd522c5ef04bfe18117381039154d881
TengSir <TengSir@live.cn>
2019-04-02 09:45:01 +0800

    添加了上传下载功能

---
+++ src/edu/hbuas/blog/control/FileDownloaderServlet.java
@@ -0,0 +1,39 @@
+package edu.hbuas.blog.control;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+
+@WebServlet(name = "FIleDownloaderServlet",urlPatterns = "/FIleDownloaderServlet")
+public class FileDownloaderServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String realName="软工培养方案.docx";
+        response.setContentType("application/msword");//mime
+        response.setHeader("Content-Disposition","attachment;filename="+ URLEncoder.encode(realName,"UTF-8"));
+
+        OutputStream  out=response.getOutputStream();
+        FileInputStream in=new FileInputStream(request.getRealPath("files")+"/3s23242sfsfdf23sdfsf.doc");
+
+        byte[] bs=new byte[1024];
+        int len=-1;
+        while((len=in.read(bs))!=-1){
+
+            out.write(bs,0,len);
+        }
+        out.flush();
+        out.close();
+        in.close();
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doPost(request, response);
+    }
+}
---
+++ web/file.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: TengSir
+  Date: 2019/4/1
+  Time: 20:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<a href="files/2019年新技术教学标准课表.xls">本站须知1</a>
+<a href="files/王大毛-去年夏天.mp3">本站须知1</a>
+<a href="FIleDownloaderServlet">本站须知1</a>
+
+</body>
+</html>
