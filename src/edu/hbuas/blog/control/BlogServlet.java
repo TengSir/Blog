a7d451dd2782f5fdf44790b26432498ca060324d
TengSir <TengSir@live.cn>
2019-04-08 21:11:27 +0800

    添加了过滤和监听器组件

--- src/edu/hbuas/blog/control/BlogServlet.java
+++ src/edu/hbuas/blog/control/BlogServlet.java
@@ -19,72 +19,72 @@ import java.util.List;
 
 @WebServlet(name = "BlogServlet",urlPatterns = "/BlogServlet")
 public class BlogServlet extends HttpServlet {
-    private BlogDAO blogDAO;
+	private BlogDAO blogDAO;
 
-    @Override
-    public void init() throws ServletException {
-        blogDAO=new BlogDAOImp();
+	@Override
+	public void init() throws ServletException {
+		blogDAO=new BlogDAOImp();
 
-    }
+	}
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("utf-8");
-        String method=request.getParameter("method");
-        switch (method){
-            case "listTopBlogs":{
-                listTopBlogs(request,response);
-                break;
-            }
-            case "getDetailOfBlogById":{
-                getDetailOfBlogById(request,response);
-                break;
-            }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("utf-8");
+		String method=request.getParameter("method");
+		switch (method){
+			case "listTopBlogs":{
+				listTopBlogs(request,response);
+				break;
+			}
+			case "getDetailOfBlogById":{
+				getDetailOfBlogById(request,response);
+				break;
+			}
 
-        }
-    }
+		}
+	}
 
-    /**
-     * 根据帖子id查询帖子详情的业务方法
-     * @param request
-     * @param response
-     * @throws ServletException
-     * @throws IOException
-     */
-    protected void getDetailOfBlogById(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String blogid=request.getParameter("blogid");
-        try {
-          Blogs b=blogDAO.getBlogDetailById(Integer.parseInt(blogid));
+	/**
+	 * 根据帖子id查询帖子详情的业务方法
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	protected void getDetailOfBlogById(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String blogid=request.getParameter("blogid");
+		try {
+		  Blogs b=blogDAO.getBlogDetailById(Integer.parseInt(blogid));
 
-            request.setAttribute("blog",b);
-            request.getRequestDispatcher("show.jsp").forward(request,response);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+			request.setAttribute("blog",b);
+			request.getRequestDispatcher("show.jsp").forward(request,response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 
-    }
+	}
 
-    /**
-     * 这个方法是查询热门博客的业务方法
-     * @param request
-     * @param response
-     * @throws ServletException
-     * @throws IOException
-     */
-    protected void listTopBlogs(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-         List<Blogs> bs=blogDAO.listBlogs();
+	/**
+	 * 这个方法是查询热门博客的业务方法
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	protected void listTopBlogs(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+		 List<Blogs> bs=blogDAO.listBlogs();
 
-            request.setAttribute("allBlogs",bs);
-            request.getRequestDispatcher("index.jsp").forward(request,response);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+			request.setAttribute("allBlogs",bs);
+			request.getRequestDispatcher("index.jsp").forward(request,response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-    }
+	}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request, response);
-    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
 }
